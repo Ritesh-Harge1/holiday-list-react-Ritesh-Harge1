84589286dcbf58d1427efcf17baebb0e810bb36c
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-06 15:42:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// Create root and render App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
